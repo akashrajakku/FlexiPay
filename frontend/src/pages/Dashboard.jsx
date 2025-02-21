865f5b80b32e07da03e150b0165d19cbdfe84beb
akashrajakku <akashrajakku263@gmail.com>
2025-02-22 05:19:50 +0530

    added footer to dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Appbar from "../components/Appbar"
 import Balance from "../components/Balance"
+import Footer from "../components/Footer";
 import Users from "../components/Users"
 import { useOutletContext } from "react-router-dom";
 
@@ -8,9 +9,12 @@ function Dashboard() {
 
   return (
     <>
+    <div className="min-h-screen">
       <Appbar user={firstName || "User"} />
       <Balance balance={"10,000"} />
       <Users />
+      </div>
+      <Footer />
     </>
   )
 }
