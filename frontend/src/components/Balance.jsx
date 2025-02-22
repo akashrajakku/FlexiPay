558478505015d8f3a1cce23cd31ec4bc18cee471
akashrajakku <akashrajakku263@gmail.com>
2025-02-22 07:18:28 +0530

    Get Balance button is created which toggles between 2 states, "shows balance" and "hides balance"

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,8 +1,11 @@
-export default function Balance({ balance }) {
-    return (
-      <div className="inline-flex sm:ml-14 ml-4 mt-6 border-2 border-gray-500 p-2 bg-blue-50 shadow-lg">
-        <div className="font-bold text-lg">Your Balance</div>
-        <div className="font-semibold text-lg ml-4">Rs. {balance}</div>
-      </div>
-    );
-  }
+export default function Balance({ balance, balanceText, flag, onClick }) {
+  return (
+    <div 
+      className="inline-flex sm:ml-14 ml-4 mt-6 border-2 border-gray-500 p-2 bg-blue-50 shadow-lg cursor-pointer" 
+      onClick={onClick} 
+    >
+      <div className="font-bold text-lg">{balanceText}</div>
+      {flag && <div className="font-semibold text-lg ml-4">Rs. {balance}</div>}
+    </div>
+  );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,15 +3,26 @@ import Balance from "../components/Balance"
 import Footer from "../components/Footer";
 import Users from "../components/Users"
 import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
 
 function Dashboard() {
   const { firstName, balance } = useOutletContext() || {};
+  const [showBalance, setShowBalance] = useState(false);
+
+  const handleToggleBalance=()=>{
+      setShowBalance((prev) => !prev)
+  }
 
   return (
     <>
     <div className="min-h-screen">
       <Appbar user={firstName || "User"} />
-      <Balance balance={balance || "Wait..."} />
+      <Balance 
+        flag={showBalance} 
+        balanceText= {showBalance?"Your Balance":"Get Balance"}
+        balance={balance || "Wait..."} 
+        onClick={handleToggleBalance}
+      />
       <Users />
       </div>
       <Footer />
